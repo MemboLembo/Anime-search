c9c8df38cba27b882387588684f51f1cec92331f
Membo Lembo <1nfinitiv6995@gmail.com>
2019-12-19 21:49:08 +0200

    First commit

---
+++ js/index.js
@@ -0,0 +1,49 @@
+'use strict';
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const userSearch = document.querySelector('.search-form__content__input');
+    const btn = document.querySelector('.search-form__content__btn');
+
+    function createItemHtml (result) {
+        const {
+            title, 
+            score, 
+            image_url: imageUrl, 
+            episodes: episodeCount
+        } = result;
+        return `<div class="api__search-result__anime-content__title">${title}</div>
+                <div class="api__search-result__anime-content__img">
+                    <img src="${imageUrl}" alt="image">
+                </div>
+                <div class="api__search-result__anime-content__bottom">
+                    <div class="api__search-result__anime-content__score">&#x2605 ${score}</div>
+                    <div class="api__search-result__anime-content__episodes">ep. ${episodeCount}</div>
+                </div>`
+    };
+
+    btn.addEventListener('click', function () {
+        const searchVar = userSearch.value;
+        console.log(searchVar);
+
+        fetch(`https://api.jikan.moe/v3/search/anime?q=${searchVar}`)
+            .then(function (resp) {
+                return resp.json()
+            })
+            .then(function (data) {
+                console.log(data);
+                const animeContent = document.querySelector('.api__search-result__anime-content');
+
+                data.results.forEach(result => {
+                    const itemElement = document.createElement('div');
+                    itemElement.classList.add('api__search-result__anime-content__item');
+                    animeContent.appendChild(itemElement);
+
+                    itemElement.innerHTML = createItemHtml(result);
+                });
+
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    });
+});
