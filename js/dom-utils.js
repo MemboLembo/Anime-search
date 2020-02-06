66e4f6cc7fc0c7a23d98e48c54b22376a8a15b73
Membo Lembo <1nfinitiv6995@gmail.com>
2020-02-06 19:06:15 +0200

    random anime, btn scroll top, scroll data

--- js/dom-utils.js
+++ js/dom-utils.js
@@ -5,5 +5,30 @@ loadingAnimation.style = display;
 
 export function setInputVal(value) {
   const userSearch = document.querySelector('.search-form__content__input');
-  userSearch.value = value;
+  userSearch.value = decodeURIComponent(value);
+}
+
+export function setErrorOutput(output) {
+  const errorsOutput = document.querySelector('.errors');
+  errorsOutput.innerText = output;
+}
+
+let scrollData, urlForCheck;
+export function setScrollTop() {
+  if (scrollData && window.location.hash === urlForCheck) {
+    document.documentElement.scrollTop = scrollData;
+  }
+}
+
+function scrollAndUrlHolder() {
+  scrollData = document.documentElement.scrollTop;
+  urlForCheck = window.location.hash;
+}
+
+export function addListenerOfScroll() {
+  window.addEventListener('scroll', scrollAndUrlHolder );
+}
+
+export function removeListenerOfScroll() {
+  window.removeEventListener('scroll', scrollAndUrlHolder );
 }
